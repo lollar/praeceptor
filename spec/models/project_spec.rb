4713abf0ff0a68247df67439b8f63f03c4d0ff34
lollar <lollar.mchl@gmail.com>
2017-04-21 01:05:51 -0600

    adds in project model specs and base model

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,13 @@
+# require 'spec_helper'
+require 'rails_helper'
+describe Project do
+  context 'initialization' do
+    specify { expect(Project.new).to be_a(Project) }
+  end
+
+  context 'database validations' do
+    project = Project.new
+    it { expect(project.save).to eq false }
+    it { expect(project.save!).to raise_error }
+  end
+end
