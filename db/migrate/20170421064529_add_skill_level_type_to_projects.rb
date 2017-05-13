88f63abafd420e96e840e4e6e9a4e2f5a0a16d30
lollar <lollar.mchl@gmail.com>
2017-05-12 23:15:39 -0600

    adds in down method for custom execute method

--- db/migrate/20170421064529_add_skill_level_type_to_projects.rb
+++ db/migrate/20170421064529_add_skill_level_type_to_projects.rb
@@ -1,12 +1,21 @@
 class AddSkillLevelTypeToProjects < ActiveRecord::Migration[5.1]
-  def change
+  def up 
     create_new_type
     add_column :projects, :skill_level, :skill_level, null: false, default: 'novice', index: true
+  end
+
+  def down
+    remove_column :projects, :skill_level
+    destroy_skill_level_type
   end
 
   private
 
   def create_new_type
     execute("CREATE TYPE skill_level AS ENUM ('novice', 'junior', 'mid-level', 'senior');")
+  end
+
+  def destroy_skill_level_type
+    execute("DROP TYPE skill_level;")
   end
 end
