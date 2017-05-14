65627410999c579ae48932ed4fd7c01adaa1f4b9
lollar <lollar.mchl@gmail.com>
2017-05-13 23:11:51 -0600

    completes spec for database validations on model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,8 +5,19 @@ describe Project do
     specify { expect(Project.new).to be_a(Project) }
   end
 
-  context 'database validations' do
-    project = Project.new
-    it { expect { project.save! }.to raise_error(ActiveRecord::NotNullViolation) }
+  context 'testing database constraints' do
+    specify { expect { Project.create!() }.to raise_error(ActiveRecord::NotNullViolation) }
+    specify { expect { Project.create!(title: 'Test Project') }.to raise_error(ActiveRecord::NotNullViolation) }
+    specify { expect { Project.create!(title: 'Test', requirements: 'All the tests', skill_level: 'wrong') }.to raise_error(ActiveRecord::StatementInvalid) }
+  end
+
+  context 'successfully create a new Project' do
+    let(:project) { Project.new(title: 'Test', requirements: 'test') }
+    let(:project2) { Project.new(title: 'Test', requirements: 'test', skill_level: 'senior') }
+
+    specify { expect(project.save).to be_truthy }
+    specify { expect(project.skill_level).to eq 'novice' }
+    specify { expect { project2.save! }.not_to raise_error }
+    specify { expect(project2.skill_level).to eq 'senior' }
   end
 end
