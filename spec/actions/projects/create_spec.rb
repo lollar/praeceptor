2112e0593b139551dffe3119abd90e1f023ab40f
lollar <lollar.mchl@gmail.com>
2017-05-24 11:31:51 -0600

    use `specify` syntax to stick with single line test convention

--- spec/actions/projects/create_spec.rb
+++ spec/actions/projects/create_spec.rb
@@ -9,21 +9,21 @@ describe Projects::Create do
   context '#run successfully creates a new project' do
     let(:result) { subject.run(valid_attributes) }
 
-    it { expect(result).to be_successful }
-    it { expect(result.message).to match(/success/i) }
-    it { expect(result.object).to be_an_instance_of(Project) }
-    it { expect(result.object).to be_valid }
-    it { expect(result.object).to be_persisted }
+    specify { expect(result).to be_successful }
+    specify { expect(result.message).to match(/success/i) }
+    specify { expect(result.object).to be_an_instance_of(Project) }
+    specify { expect(result.object).to be_valid }
+    specify { expect(result.object).to be_persisted }
   end
 
   context '#run fails to complete but does not raise exception' do
     let(:result) { subject.run(invalid_attributes) }
 
-    it { expect(result).not_to be_successful }
-    it { expect(result).to be_failed }
-    it { expect(result.message).to match(/blah/) }
-    it { expect(result.object).to be_an_instance_of(Project) }
-    it { expect(result.object).not_to be_valid }
-    it { expect(result.object).not_to be_persisted }    
+    specify { expect(result).not_to be_successful }
+    specify { expect(result).to be_failed }
+    specify { expect(result.message).to match(/blah/) }
+    specify { expect(result.object).to be_an_instance_of(Project) }
+    specify { expect(result.object).not_to be_valid }
+    specify { expect(result.object).not_to be_persisted }    
   end
 end
